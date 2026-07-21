d90056d64f0a262af8e340f9366db96a9e0da1ae
sukritta5447 <75234379+sukritta5447@users.noreply.github.com>
2026-07-21 15:26:36 +0700

    chore: update dependencies and database connection string

--- server/utils/db.mjs
+++ server/utils/db.mjs
@@ -4,7 +4,7 @@ const { Pool } = pg.default;
 
 const connectionPool = new Pool({
   connectionString:
-    "postgresql://your-db-username:your-db-password@localhost:5432/your-db-name",
+    "postgresql://postgres:Jamesvg6661@localhost:5432/build_api_assignment",
 });
 
 export default connectionPool;
