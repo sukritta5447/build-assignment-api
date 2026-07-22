3b326ead8751891b984abfac396f5c0c66fd51d4
sukritta5447 <75234379+sukritta5447@users.noreply.github.com>
2026-07-22 14:12:44 +0700

    feat: implement CRUD operations for assignments

--- server/app.mjs
+++ server/app.mjs
@@ -25,6 +25,70 @@ app.post("/assignments", async (req, res) => {
     }
 });
 
+// User สามารถดูข้อมูลแบบทดสอบทั้งหมดในระบบได้
+app.get("/assignments", async (req, res) => {
+  try {
+    const result = await connectionPool.query("SELECT assignment_id AS id, title, content, category FROM assignments");
+    return res.status(200).json({ data: result.rows });
+  } catch (e) {
+    return res.status(500).json({ 
+      "message": "Server could not read assignment because database connection" 
+    });
+  }
+});
+
+// User สามารถดูข้อมูลแบบทดสอบอันเดียวได้
+app.get("/assignments/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const result = await connectionPool.query("SELECT assignment_id AS id, title, content, category FROM assignments WHERE assignment_id = $1", [id]);
+    if (result.rows.length === 0) {
+      return res.status(404).json({ "message": "Server could not find a requested assignment" });
+    }
+    return res.status(200).json({ data: result.rows[0] });
+  } catch (e) {
+    return res.status(500).json({ 
+      "message": "Server could not read assignment because database connection" 
+    });
+  }
+});
+
+// User สามารถแก้ไขแบบทดสอบที่ได้เคยสร้างไว้ก่อนหน้านี้
+app.put("/assignments/:id", async (req, res) => {
+  const { id } = req.params;
+  const { title, content, category } = req.body;
+
+  if (!title || !content || !category) {
+    return res.status(400).json({ "message": "Server could not find a requested assignment to update" });
+  }
+
+  try {
+    const result = await connectionPool.query("UPDATE assignments SET title = $1, content = $2, category = $3 WHERE assignment_id = $4 RETURNING *", [title, content, category, id]);
+    if (result.rows.length === 0) {
+      return res.status(404).json({ "message": "Server could not find a requested assignment to update" });
+    }
+    return res.status(200).json({ "message": "Updated assignment successfully" });
+  } catch (e) {
+    return res.status(500).json({ "message": "Server could not update assignment because database connection" });
+  }
+});
+
+// User สามารถลบแบบทดสอบที่ได้เคยสร้างไว้ก่อนหน้านี้
+app.delete("/assignments/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const result = await connectionPool.query("DELETE FROM assignments WHERE assignment_id = $1 RETURNING *", [id]);
+    if (result.rows.length === 0) {
+      return res.status(404).json({ "message": "Server could not find a requested assignment to delete" });
+    }
+    return res.status(200).json({ "message": "Deleted assignment successfully" });
+  } catch (e) {
+    return res.status(500).json({ "message": "Server could not delete assignment because database connection" });
+  }
+});
+
 app.get("/test", (req, res) => {
   return res.json("Server API is working 🚀");
 });
